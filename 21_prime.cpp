988eec5f8fa10a455de6031da9288bd706443b35
Md.Firoj Mahmud <162411789+firoj04@users.noreply.github.com>
2024-03-12 06:54:58 +0600

    Add files via upload

---
+++ 21_prime.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+class prime{
+    int x;
+    public:
+        void get(){
+            cout<<"Enter the value of x: ";
+            cin>>x;
+        }
+        void check(){
+            if(x%2==0){
+                cout<< "YES"<<endl;
+            }
+            else{
+                cout<< "False"<<endl;
+            }
+        }
+};
+int main(){
+    prime p;
+    p.get();
+    p.check();
+}
