8daae6d7498f986da64646394b4041685478d6fe
Md.Firoj Mahmud <162411789+firoj04@users.noreply.github.com>
2024-03-12 06:34:01 +0600

    Add files via upload

---
+++ 14_Basic_input_output.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class tests{
+    int x = 10;
+    public:
+    void display(){
+        cout<<"The vaule of X is: "<<x<<endl;
+    }
+};
+int main(){
+    tests t;
+    t.display();
+}
