4f1b03d3dc69cc68699b96a92081a6b748f16d14
Md.Firoj Mahmud <162411789+firoj04@users.noreply.github.com>
2024-03-12 07:11:35 +0600

    Add files via upload

---
+++ 25_static_data_function.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+class test{
+    public:
+        static void show(){
+            cout<<"I am Static Data Function......";
+        }
+};
+int main(){
+    test::show();
+}
